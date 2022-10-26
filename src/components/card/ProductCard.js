72e3edc9a7a221ed6753bc28149b4a4571d104d6
ujangpudin04 <ujangpudin04@gmail.com>
2022-10-26 21:29:43 +0700

    edit file

--- src/components/card/ProductCard.js
+++ src/components/card/ProductCard.js
@@ -14,19 +14,18 @@ export default function ProductCard({ item }) {
           borderRadius: "10px",
         }}
       >
-        <div class="card h-100">
-          <img
-            src={item.image}
-            className="img-fluid img-rounded"
-            alt={item.name}
-          />
-          <div className="p-2">
-            <div className="text-header-product-item">{item.name}</div>
-            <div className="text-product-item">
-              {convertRupiah.convert(item.price)}
-            </div>
-            <div className="text-product-item">Stock : {item.qty}</div>
+        <img
+          src={item.image}
+          className="img-fluid img-rounded"
+          alt={item.name}
+          style={{ width: "200px", height: "200px" }}
+        />
+        <div className="p-2">
+          <div className="text-header-product-item">{item.name}</div>
+          <div className="text-product-item">
+            {convertRupiah.convert(item.price)}
           </div>
+          <div className="text-product-item">Stock : {item.qty}</div>
         </div>
       </div>
     </Link>
